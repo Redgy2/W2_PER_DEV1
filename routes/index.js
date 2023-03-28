e645d208cdb36911f3cc00d30ed2c262d762f44b
Redgy2 <lil_redgyperard@hotmail.com>
2023-03-27 21:57:09 -0400

    TP3

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,28 @@ router.get('/', (requete, reponse) => {
     reponse.send('utilisez /api/messages pour faire appel au service Web des messages...');
 });
 
+router.get('/api/aide', (requete, reponse) => {
+    reponse.send('Pour plus de details de comment utiliser un API et la documentation aller sur le site : "https://docs.github.com/en/rest"');
+});
+
+router.get('/index.html', (requete, reponse) => {
+    lirePageWeb('index.html', reponse);
+});
+
+function lirePageWeb(nomPageWeb, reponse) {
+    const fs = require('fs');
+    const path = require('path');
+    const fichier = path.join(__dirname, "..", nomPageWeb);
+    console.log(fichier);
+    fs.readFile(fichier, (err, contenu) => {
+        if (err) {
+            reponse.status(500).send('Erreur serveur Web: '+err.code);
+        } else {
+            reponse.status(200).set( {'Content-type': 'text/html'} ).send(contenu);
+        }
+    });
+}
+
 module.exports = router;
 
 
